a219d3d722d13075b840d9dc385b3fe0b6cab88c
LexRillo <fearsless@hotmail.it>
2017-04-08 03:39:47 +0200

    more changes

--- src/CO2017/exercise3/acs49/MessageClient.java
+++ src/CO2017/exercise3/acs49/MessageClient.java
@@ -7,8 +7,7 @@ public class MessageClient {
 
 	public MessageClient(){}
 	
-	static void	main(String[] args){
-		
+	static void	main(String[] args){		
 //		Main client behaviour.
 //		Use the command line arguments to open a connection to a Message Server.
 //
